189e80eea7754c935c829f22e0b059d1ea4b6876
MihaiMijea <mijeamihai@yahoo.com>
2014-12-19 07:04:19 +0100

    First Stage

---
+++ censorify/censortext.js
@@ -0,0 +1,21 @@
+var censoreWords = ["sad", "bad", "mad"];
+var customCensoredVords = [];
+function censor(inStr){
+	for (idx in censoredWords){
+		inStr = inStr.replace(censoredWords[idx], "****");
+	}
+	for (idx in customCensoredVords){
+		inStr = inStr.replace(customCensoredVords[idx], "****");
+	}
+	return inStr;
+}
+function addCensoredWord(word){
+	customCensoredVords.push(word);
+}
+function getCensoredWords(){
+	return censoredWords.concat(customCensoredVords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
