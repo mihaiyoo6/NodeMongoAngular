854af7034b8fc744cea14c903204b11d907a8035
MihaiMijea <mijeamihai@yahoo.com>
2015-02-15 07:27:03 +0100

    Chapter 9

---
+++ Chapter09/child_process_spawn_file.js
@@ -0,0 +1,13 @@
+var spawn = require('child_process').spawn;
+var options = {
+	env:{user: 'Mihai'},
+	detached: false,
+	stdio:['pipe', 'pipe', 'pipe']
+};
+var child = spawn('netstat', ['-e']);
+child.stdout.on('data', function(data){
+	console.log(data.toString());
+});
+child.on('exit', function(code){
+	console.log('Child exited with code ', code);
+});
